839dbc775e64e2a47ee7f17c29131cba07e56aab
Hue Pham <huepham113@gmail.com>
2021-02-27 18:02:01 -0500

    Added feature to enter focus task, set timer, countdown

---
+++ src/components/Countdown.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+import { fontSizes, spacing } from "../utils/sizes";
+import { colors } from "../utils/colors";
+
+const minutesToMillis = (min) => min * 1000 * 60;
+const formatTime = (time) => (time < 10 ? `0${time}` : time);
+
+export const Countdown = ({ minutes = 1, isPaused, onProgress, onEnd }) => {
+  const [millis, setMillis] = useState(null);
+  const interval = React.useRef(null);
+
+  const countDown = () => {
+    setMillis((time) => {
+      if (time === 0) {
+        clearInterval(interval.current);
+        onEnd();
+        return time;
+      }
+      console.log("time", time);
+      const timeLeft = time - 1000;
+      onProgress(timeLeft / minutesToMillis(minutes));
+      return timeLeft;
+    });
+  };
+
+  useEffect(() => {
+    if (isPaused) {
+      if (interval.current) clearInterval(interval.current);
+      return;
+    }
+    interval.current = setInterval(countDown, 1000);
+
+    return () => clearInterval(interval.current);
+  }, [isPaused]);
+
+  useEffect(() => {
+    setMillis(minutesToMillis(minutes));
+  }, [minutes]);
+
+  useEffect(() => {
+    // onProgress(millis / minutesToMillis(minutes));
+    console.log("Countdown millis", millis);
+  }, [millis]);
+
+  const minute = Math.floor(millis / 1000 / 60) % 60;
+  const seconds = Math.floor(millis / 1000) % 60;
+
+  return (
+    <Text style={styles.text}>
+      {formatTime(minute)}:{formatTime(seconds)}
+    </Text>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: fontSizes.xxxl,
+    fontWeight: "bold",
+    color: colors.white,
+    padding: spacing.lg,
+    backgroundColor: "rgba(94, 132, 226, 0.3)",
+  },
+});
---
+++ src/utils/sizes.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const fontSizes = {
+  sm: 8,
+  md: 16,
+  lg: 24,
+  xl: 32,
+  xxl: 40,
+  xxxl: 80,
+};
+
+export const spacing = {
+  sm: 8,
+  md: 16,
+  lg: 24,
+  xl: 32,
+  xxl: 40,
+  xxxl: 80,
+};
